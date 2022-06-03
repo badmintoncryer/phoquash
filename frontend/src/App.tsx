b643fe60586410d1ffc8d4c44876a1133ea67b03
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-06-03 21:02:27 +0900

    [added] facebook連携

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,11 +8,21 @@ Amplify.configure({
   aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
   aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
   aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_CLIENT_ID,
+  oauth: {
+    domain: "phoquash.auth.us-west-2.amazoncognito.com",
+    // scope: ["openid", "aws.cognito.signin.user.admin", "profile"],
+    redirectSignIn: "https://localhost:3000",
+    redirectSignOut: "https://localhost:3000",
+    responseType: "code",
+  },
 });
 
 export default function App() {
   return (
-    <Authenticator socialProviders={["facebook"]}>
+    <Authenticator
+      socialProviders={["facebook"]}
+      loginMechanisms={["username"]}
+    >
       {({ signOut, user }) => (
         <main>
           {user ? <h1>Hello {user.username}</h1> : <h1>User is not defined</h1>}
