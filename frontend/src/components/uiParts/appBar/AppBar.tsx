762f5d0f97761d384742f7dbcef7bf91be813aac
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-06-05 16:16:22 +0900

    [updated] ロゴ表示

--- frontend/src/components/uiParts/appBar/AppBar.tsx
+++ frontend/src/components/uiParts/appBar/AppBar.tsx
@@ -9,6 +9,7 @@ import MenuItem from "@mui/material/MenuItem";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 import { Grid } from "@mui/material";
+import Logo from "../../../static/logo_transparent.png";
 
 export default function ButtonAppBar() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -34,38 +35,43 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-butoon",
-            }}
-          >
-            <MenuItem onClick={handleToTopPage}>HOMEへ戻る</MenuItem>
-            <MenuItem onClick={handleCreate}>新規作成</MenuItem>
-            <MenuItem disabled onClick={handleToTopPage}>
-              管理画面
-            </MenuItem>
-            <MenuItem disabled onClick={handleToTopPage}>
-              ログアウト
-            </MenuItem>
-          </Menu>
-          <Grid container direction="column">
+          <Grid container direction="row" alignItems="center">
+            <Grid item>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-butoon",
+                }}
+              >
+                <MenuItem onClick={handleToTopPage}>HOMEへ戻る</MenuItem>
+                <MenuItem onClick={handleCreate}>新規作成</MenuItem>
+                <MenuItem disabled onClick={handleToTopPage}>
+                  管理画面
+                </MenuItem>
+                <MenuItem disabled onClick={handleToTopPage}>
+                  ログアウト
+                </MenuItem>
+              </Menu>
+            </Grid>
+            <Grid item>
+              <img src={Logo} alt="logo" width={50} />
+            </Grid>
             <Grid item>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 phoquash
