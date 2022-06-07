ac2ee1fab62bf593602940225d822ee553dd0131
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-06-07 21:46:58 +0900

    [added] cardの見た目

--- frontend/src/components/pages/home/Home.tsx
+++ frontend/src/components/pages/home/Home.tsx
@@ -1,3 +1,53 @@
+import { Box, Container, Grid } from "@mui/material";
+import { TravelRecordCard } from "components/uiParts/TravelRecordCard/TravelRecordCard";
+import { useState } from "react";
+
+type TravelRecord = {
+  title: string;
+  start: Date;
+  end: Date;
+};
+
 export const Home = () => {
-  return <div></div>;
+  const [travelList, setTravelList] = useState<TravelRecord[]>([
+    {
+      title: "愛媛・高知",
+      start: new Date("1995-12-17"),
+      end: new Date("1995-12-21"),
+    },
+    {
+      title: "クロアチア",
+      start: new Date("2021-12-10"),
+      end: new Date("2021-12-15"),
+    },
+  ]);
+
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 4,
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          container
+          direction="column"
+          spacing={1}
+          justifyContent="flex-start"
+          alignItems="stretch"
+        >
+          {travelList.map((travel) => (
+            <Grid item key={travel.title} xs={12}>
+              <TravelRecordCard
+                title={travel.title}
+                start={travel.start}
+                end={travel.end}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  );
 };
--- frontend/src/components/uiParts/TravelRecordCard/TravelRecordCard.tsx
+++ frontend/src/components/uiParts/TravelRecordCard/TravelRecordCard.tsx
@@ -1,15 +1,31 @@
-import { Card, CardHeader, CardMedia } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+} from "@mui/material";
 
-export const TravelRecordCard = () => {
+export type TravelRecordCardProps = {
+  title: string;
+  start: Date;
+  end: Date;
+};
+
+export const TravelRecordCard = (props: TravelRecordCardProps) => {
+  const period = `${props.start.toLocaleDateString()} - ${props.end.toLocaleDateString()}`;
   return (
-    <Card>
-      <CardHeader title="愛媛・高知" subheader="2022/05/01 - 2022/05/04" />
+    <Card raised={true}>
+      <CardHeader title={props.title} subheader={period} />
       <CardMedia
         component="img"
         height="140"
         image="https://user-images.githubusercontent.com/64848616/172041210-2901aeb3-dcbf-423d-9ab8-eb311a4e3202.JPG"
-        alt="test picture"
+        alt={props.title}
       />
+      <CardActions>
+        <Button size="small">Share</Button>
+      </CardActions>
     </Card>
   );
 };
