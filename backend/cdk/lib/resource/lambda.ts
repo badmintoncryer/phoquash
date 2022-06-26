d2e96ce4f8d202e199e4ff80c5c911476a6432f9
kazuho cryershinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-06-26 23:15:15 +0900

    [added] sqlite3 lambda

--- backend/cdk/lib/resource/lambda.ts
+++ backend/cdk/lib/resource/lambda.ts
@@ -1,26 +1,44 @@
 import { Construct } from "constructs";
 import * as efs from "aws-cdk-lib/aws-efs";
-import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as path from "path";
 
 export class Lambda {
   public postTravelRecordLambda: lambda.Function;
-  private readonly accessPoint: efs.AccessPoint
+  public nodeLayer: lambda.LayerVersion;
+  private readonly accessPoint: efs.AccessPoint;
   private readonly vpc: ec2.Vpc;
 
   constructor(accessPoint: efs.AccessPoint, vpc: ec2.Vpc) {
-    this.accessPoint = accessPoint
-    this.vpc = vpc
+    this.accessPoint = accessPoint;
+    this.vpc = vpc;
   }
 
   public createResources(scope: Construct) {
-    this.postTravelRecordLambda = new lambda.Function(scope, 'postTravelRecordLambda', {
-      filesystem: lambda.FileSystem.fromEfsAccessPoint(this.accessPoint, "/mnt/db"),
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/postTravelRecord')),
-      vpc: this.vpc
-    })
+    this.nodeLayer = new lambda.LayerVersion(scope, "nodeLayer", {
+      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/layer")),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_16_X],
+      description: "node layer",
+      layerVersionName: "node-layer",
+    });
+
+    this.postTravelRecordLambda = new lambda.Function(
+      scope,
+      "postTravelRecordLambda",
+      {
+        filesystem: lambda.FileSystem.fromEfsAccessPoint(
+          this.accessPoint,
+          "/mnt/db"
+        ),
+        layers: [this.nodeLayer],
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler: "index.handler",
+        code: lambda.Code.fromAsset(
+          path.join(__dirname, "../lambda/function/postTravelRecord")
+        ),
+        vpc: this.vpc,
+      }
+    );
   }
 }
