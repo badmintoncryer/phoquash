c2b86968aefe9087c2d2a6d3eb6a0af92aa5caa6
kazuho cryer-shinozuka <cryershinozuka.kb@om.asahi-kasei.co.jp>
2022-08-12 15:05:22 +0900

    fix: 一旦リソース削除

--- backend/cdk/lib/cdk-stack.ts
+++ backend/cdk/lib/cdk-stack.ts
@@ -1,11 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { Cognito } from './resource/cognito'
-import { Efs } from './resource/efs'
+// import { Efs } from './resource/efs'
 import { Vpc } from './resource/vpc'
-import { Lambda } from './resource/lambda'
-import { ApiGateway } from './resource/apiGateway'
-import { S3 } from './resource/s3'
+// import { Lambda } from './resource/lambda'
+// import { ApiGateway } from './resource/apiGateway'
+// import { S3 } from './resource/s3'
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,34 +14,34 @@ export class CdkStack extends Stack {
     const vpc = new Vpc()
     vpc.createResources(this)
 
-    const efs = new Efs(vpc.vpc)
-    efs.createResources(this)
+    // const efs = new Efs(vpc.vpc)
+    // efs.createResources(this)
 
-    const lambda = new Lambda(efs.accessPoint, vpc.vpc)
-    lambda.createResources(this)
+    // const lambda = new Lambda(efs.accessPoint, vpc.vpc)
+    // lambda.createResources(this)
 
-    const s3 = new S3(lambda.uploadPhotoDataLambda)
-    s3.createResources(this)
+    // const s3 = new S3(lambda.uploadPhotoDataLambda)
+    // s3.createResources(this)
 
     const cognito = new Cognito()
     cognito.createResources(this)
 
-    const apigw = new ApiGateway(
-      cognito.userPool,
-      cognito.userPoolClient,
-      lambda.postUserLambda,
-      lambda.deleteUserLambda,
-      lambda.deleteUserByIdLambda,
-      lambda.getUserByIdLambda,
-      lambda.postTravelRecordLambda,
-      lambda.deleteTravelRecordLambda,
-      lambda.deleteTravelRecordByIdLambda,
-      lambda.postTravelLambda,
-      lambda.deleteTravelLambda,
-      lambda.deleteTravelByIdLambda,
-      lambda.getTravelByIdLambda,
-      lambda.postPhotoLambda
-    )
-    apigw.createResources(this)
+    // const apigw = new ApiGateway(
+    //   cognito.userPool,
+    //   cognito.userPoolClient,
+    //   lambda.postUserLambda,
+    //   lambda.deleteUserLambda,
+    //   lambda.deleteUserByIdLambda,
+    //   lambda.getUserByIdLambda,
+    //   lambda.postTravelRecordLambda,
+    //   lambda.deleteTravelRecordLambda,
+    //   lambda.deleteTravelRecordByIdLambda,
+    //   lambda.postTravelLambda,
+    //   lambda.deleteTravelLambda,
+    //   lambda.deleteTravelByIdLambda,
+    //   lambda.getTravelByIdLambda,
+    //   lambda.postPhotoLambda
+    // )
+    // apigw.createResources(this)
   }
 }
